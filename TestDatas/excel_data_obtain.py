a8f3973b59d5e9d1b1f5120fd8bfa88517b0ce02
liuning <2455899418@qq.com>
2020-08-07 15:11:05 +0800

    登录用例数据获取及处理
    Signed-off-by: liuning <2455899418@qq.com>

--- TestDatas/excel_data_obtain.py
+++ TestDatas/excel_data_obtain.py
@@ -34,7 +34,7 @@ class read_test_datas:
 
 
 test_table_name = "TestSystem_api_Testcase.xlsx"
-sheet_name_list = ["register", "isregister", "is_email_register"]
+sheet_name_list = ["register", "isregister", "is_email_register", "login"]
 test_register = read_test_datas(test_table_name, sheet_name_list[0])
 test_register_datas = test_register.obtain_datas()
 
@@ -44,28 +44,34 @@ test_isregister_datas = test_isregister.obtain_datas()
 test_is_email_register = read_test_datas(test_table_name, sheet_name_list[2])
 test_is_email_register_datas = test_is_email_register.obtain_datas()
 
+test_login = read_test_datas(test_table_name, sheet_name_list[3])
+test_login_datas = test_login.obtain_datas()
 if __name__ == '__main__':
     from Common.handle_random_username_email_password import get_new_email, get_password, get_new_username
     from Common.handle_case_relpace_data import replace_mark_with_data
     from Common.handle_conf import red_conf
-    REPLACE_NOREGISTER_EMAIL_MARK = "#email#"
-    REPLACE_REGISTER_EMAIL_MARK = "$email$"
-    # REPLACE_PASSWORD = "#password#"
-    # REPLACE_EMAIL = "#email#"
-    # REPLACE_ID = "#id#"
-    # pprint(test_isregister_datas)
-    case = test_is_email_register_datas
+    from Common.handle_db import HandleDB
+    REPLACE_NO_REGISTER_USERNAME_MARK = "#username#"
+    REPLACE_NO_REGISTER_PASSWORD_MARK = "#password#"
+    REPLACE_REGISTER_USERNAME_MARK = "$username$"
+    REPLACE_REGISTER_PASSWORD_MARK = "$password$"
+    REPLACE_USER_ID_MARK = "$user_id$"
+    case = test_login_datas
+    # pprint(cases)
+    register_username = red_conf.get("register_user", "username")
+    register_password = red_conf.get("register_user", "password")
+    db = HandleDB()
     for cases in case:
-        if cases["url"].find(REPLACE_NOREGISTER_EMAIL_MARK) != -1:
-            email = get_new_email()
-    #         password = get_password(int(cases["random_password_len"]))
-    #         email = get_new_email()
-            cases = replace_mark_with_data(cases, REPLACE_NOREGISTER_EMAIL_MARK, email)
-    #         cases = replace_mark_with_data(cases, REPLACE_PASSWORD, password)
-    #         cases = replace_mark_with_data(cases, REPLACE_EMAIL, email)
-        elif cases["url"].find(REPLACE_REGISTER_EMAIL_MARK) != -1:
-            email = red_conf.get("register_user", "email")
-            cases = replace_mark_with_data(cases, REPLACE_REGISTER_EMAIL_MARK, email)
-        else:
-            pass
+        if cases["request_data"].find(REPLACE_REGISTER_USERNAME_MARK) != -1 or cases["request_data"].find(
+                REPLACE_REGISTER_PASSWORD_MARK) != -1 or cases["request_data"].find(REPLACE_NO_REGISTER_USERNAME_MARK) \
+                != -1 or cases["request_data"].find(REPLACE_NO_REGISTER_PASSWORD_MARK) != -1:
+            no_register_username = get_new_username(int(cases['random_username_len']))
+            no_register_password = get_password(int(cases['random_password_len']))
+            cases = replace_mark_with_data(cases, REPLACE_REGISTER_USERNAME_MARK, register_username)
+            cases = replace_mark_with_data(cases, REPLACE_REGISTER_PASSWORD_MARK, register_password)
+            cases = replace_mark_with_data(cases, REPLACE_NO_REGISTER_USERNAME_MARK, no_register_username)
+            cases = replace_mark_with_data(cases, REPLACE_NO_REGISTER_PASSWORD_MARK, no_register_password)
+        if cases['check_sql']:
+            user_id = db.select_one_data(cases['check_sql'])["id"]
+            cases = replace_mark_with_data(cases, REPLACE_USER_ID_MARK, str(user_id))
     pprint(case)
